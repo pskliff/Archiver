f24a1b984bb4c845f5f91a6b8ce64ec7e385b900
pskliff <bronkout@yahoo.com>
2018-03-15 22:41:41 +0300

    Added LZ77 algorithm

--- EncoderDecoder.h
+++ EncoderDecoder.h
@@ -2,10 +2,165 @@
 #include <codecvt>
 #include "huffman.h"
 #include "fano.h"
+#include "lz77.h"
 
 
 namespace EncDec
 {
+
+    // format: all_size.size.offssize.offs.lensize.len.char
+//    void write_lz77(std::vector<Node>& v, std::string destination_path)
+//    {
+//        std::ofstream fout(destination_path, ios::out);
+////        std::fstream fout;
+////        fout.open(destination_path, std::ios::out);
+//        int all_size = 0, offset_size = 0, len_size = 0;
+////        fout << v.size() << "\n";
+//
+//
+//        std::string all_str = std::to_string(v.size());
+//        all_size = all_str.length();
+//        char const *pchar = all_str.c_str();
+//
+//        fout.put((uchar)all_size);
+//        for (int i = 0; i < all_size; ++i)
+//        {
+//            uchar ch = pchar[i];
+//            fout.put(ch);
+//        }
+//
+//
+//        for (int i = 0; i < v.size(); i++)
+//        {
+//            std::string offs_str = std::to_string(v[i].offs);
+//            offset_size = offs_str.length();
+//            char const *offs_char = offs_str.c_str();
+//
+//            fout.put((uchar)offset_size);
+//            for (int j = 0; j < offset_size; ++j)
+//                fout.put(offs_char[j]);
+//
+//
+//
+//            std::string len_str = std::to_string(v[i].len);
+//            len_size = len_str.length();
+//            char const *len_char = len_str.c_str();
+//
+//            fout.put((uchar)len_size);
+//            for (int j = 0; j < len_size; ++j)
+//                fout.put(len_char[j]);
+//
+//
+//
+//            fout.put(v[i].ch);
+////            fout << v[i].offs<< v[i].len << v[i].ch;
+//        }
+//
+//        fout.close();
+//
+//    }
+
+
+    // allsize.div.mod.ch.div.mod.ch.......allsize times
+    void write_lz77(std::vector<Node>& v, std::string destination_path)
+    {
+        std::ofstream fout(destination_path, ios::out);
+//        std::fstream fout;
+//        fout.open(destination_path, std::ios::out);
+        uchar div = 0, mod = 0;
+//        fout << v.size() << "\n";
+
+
+//        div = v.size() / 256;
+//        mod = v.size() % 256;
+//        fout.put(div);
+//        fout.put(mod);
+        int sz = v.size();
+        int szof = sizeof(sz);
+
+        fout.write(reinterpret_cast<const char *>(&sz), sizeof(sz));
+        for (int i = 0; i < v.size(); i++)
+        {
+            div = v[i].offs / 256;
+            mod = v[i].offs % 256;
+            fout.put(div);
+            fout.put(mod);
+
+            div = v[i].len / 256;
+            mod = v[i].len % 256;
+            fout.put(div);
+            fout.put(mod);
+
+            fout.put(v[i].ch);
+//            fout << v[i].offs<< v[i].len << v[i].ch;
+        }
+
+        fout.close();
+
+    }
+
+
+    vector<uchar> decLZ77(vector<Node> arr, int n)
+    {
+        vector<uchar> res;
+        for (int i = 0; i < n; i++)
+        {
+            const Node& node = arr[i];
+            int ind = res.size() - node.offs;
+
+            // add offset symbol len times
+            for (int j = 0; j < node.len; ++j)
+                res.emplace_back(res[ind + j]);
+
+            // add next symbol
+            res.emplace_back(node.ch);
+        }
+        return res;
+    }
+
+    void decode_lz77(std::string source_path, std::string destination_path)
+    {
+        if (access(source_path.c_str(), F_OK) == -1)
+            throw std::runtime_error("File does't exist (decode_lz77)");
+
+
+        std::ifstream in(source_path, std::ios::in | std::ios::binary);
+        int div = 0, mod = 0, n = 0, offset, len;
+
+
+        in.read(reinterpret_cast<char *>(&n), sizeof(n));
+//        div = in.get();
+//        mod = in.get();
+//        n = div * 256 + mod;
+        uchar ch;
+        vector<Node> nodes;
+        for (int i = 0; i < n; ++i)
+        {
+            div = in.get();
+            mod = in.get();
+            offset = div * 256 + mod;
+
+            div = in.get();
+            mod = in.get();
+            len = div * 256 + mod;
+
+            ch = in.get();
+
+            nodes.emplace_back(Node(offset, len, ch));
+        }
+        in.close();
+
+        vector<uchar> result = decLZ77(nodes, n);
+
+
+        std::ofstream fout(destination_path, ios::out);
+        for (int i = 0; i < result.size(); ++i)
+        {
+            fout.put(result[i]);
+        }
+
+        fout.close();
+    }
 
 
 /**
--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ const string HUFF_ENCODING_EXTENCION = ".haff";
 const string HUFF_DECODING_EXTENCION = ".unhaff";
 const string FANO_ENCODING_EXTENCION = ".shan";
 const string FANO_DECODING_EXTENCION = ".unshan";
+const string LZ775_ENCODING_EXTENCION = ".lz775";
+const string LZ775_DECODING_EXTENCION = ".unlz775";
+const string LZ7710_ENCODING_EXTENCION = ".lz7710";
+const string LZ7710_DECODING_EXTENCION = ".unlz7710";
+const string LZ7720_ENCODING_EXTENCION = ".lz7720";
+const string LZ7720_DECODING_EXTENCION = ".unlz7720";
+const int HIST_BUF_MAX_4 = 1024 * 4;
+const int PREV_BUF_MAX_1 = 1024;
+const int HIST_BUF_MAX_8 = 1024 * 8;
+const int PREV_BUF_MAX_2 = 1024 * 2;
+const int HIST_BUF_MAX_16 = 1024 * 16;
+const int PREV_BUF_MAX_4 = 1024 * 4;
 
 
 /**
@@ -75,10 +87,11 @@ std::map<uchar, string> create_huffman_codes(std::string path)
 std::map<uchar, string> create_fano_codes(std::string path)
 {
     auto freqs = get_frequency(path);
+
+    // create and sort a vector of frequencies
     std::vector<std::pair<uchar, int>> freq_vec;
     for (auto iterator = freqs.begin(); iterator != freqs.end(); ++iterator)
         freq_vec.emplace_back(iterator->first, iterator->second);
-
     auto cmp = [](std::pair<uchar, int> const & a, std::pair<uchar, int> const & b)
     {
         return a.second != b.second ?  a.second > b.second : a.first > b.first;
@@ -94,16 +107,41 @@ std::map<uchar, string> create_fano_codes(std::string path)
 
 }
 
+std::vector<Node> create_lz77_codes(std::string path, int histBufMax, int prevBufMax)
+{
+    if (access(path.c_str(), F_OK) == -1)
+        throw std::runtime_error("File does't exist (create lz77)");
+
+
+    std::ifstream f(path, std::ios::in | std::ios::binary);
+    std::vector<uchar> bytes;
+    std::vector<Node> result;
+    char ch_buf;
+    while (f.get(ch_buf))
+    {
+        uchar ch = ch_buf;
+
+        // increment frequency for current char
+        bytes.emplace_back(ch);
+    }
+    f.close();
+
+    encode_LZ77(bytes, result, histBufMax, prevBufMax);
+    return result;
+}
 
 int main()
 {
-    std::string source_path = "samples/14";
-    std::map<uchar, string> huff_codes = create_huffman_codes(source_path);
-    std::map<uchar, string> fano_codes = create_fano_codes(source_path);
-    EncDec::encode_file(source_path, source_path + HUFF_ENCODING_EXTENCION, huff_codes);
-    EncDec::decode_file(source_path + HUFF_ENCODING_EXTENCION, source_path + HUFF_DECODING_EXTENCION);
-    EncDec::encode_file(source_path, source_path + FANO_ENCODING_EXTENCION, fano_codes);
-    EncDec::decode_file(source_path + FANO_ENCODING_EXTENCION, source_path + FANO_DECODING_EXTENCION);
+    std::string source_path = "samples/32";
+//    std::map<uchar, string> huff_codes = create_huffman_codes(source_path);
+//    std::map<uchar, string> fano_codes = create_fano_codes(source_path);
+//    EncDec::encode_file(source_path, source_path + HUFF_ENCODING_EXTENCION, huff_codes);
+//    EncDec::decode_file(source_path + HUFF_ENCODING_EXTENCION, source_path + HUFF_DECODING_EXTENCION);
+//    EncDec::encode_file(source_path, source_path + FANO_ENCODING_EXTENCION, fano_codes);
+//    EncDec::decode_file(source_path + FANO_ENCODING_EXTENCION, source_path + FANO_DECODING_EXTENCION);
+    std::vector<Node> lz77_codes = create_lz77_codes(source_path, HIST_BUF_MAX_4, PREV_BUF_MAX_1);
 
+    EncDec::write_lz77(lz77_codes, source_path + LZ775_ENCODING_EXTENCION);
+    EncDec::decode_lz77(source_path + LZ775_ENCODING_EXTENCION, source_path + LZ775_DECODING_EXTENCION);
     return 0;
 }
